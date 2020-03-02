64dcc8362e28b4710a0d989818206403b1d42a7e
Jose Sicajan <jlsicajan@gmail.com>
2020-03-02 06:19:58 -0600

    Mobile improvements

--- resources/views/sections/section_two.blade.php
+++ resources/views/sections/section_two.blade.php
@@ -1,10 +1,6 @@
-<div class="row vh100 section_two">
-    <div class="col-md-12 padding-50-px">
-        <div class="row">
-            <div class="col-md-12 text-center">
-                <h2><strong>HABITACIONES</strong></h2>
-                <h3 class="padding-unset text_secondary_color">Elige la mejor comodidad</h3>
-            </div>
-        </div>
+<div class="row vh100 section_two padding-50-px text-center">
+    <div class="col-md-12">
+        <h2><strong>HABITACIONES</strong></h2>
+        <h3 class="padding-unset text_secondary_color">Elige la mejor comodidad</h3>
     </div>
 </div>
