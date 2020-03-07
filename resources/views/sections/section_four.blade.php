0b6f6d4c0c1e2f2a4c3ec2dee02a26a0a4a53392
Jose Sicajan <jlsicajan@gmail.com>
2020-03-07 02:10:17 -0600

    Improvements

--- resources/views/sections/section_four.blade.php
+++ resources/views/sections/section_four.blade.php
@@ -1,7 +1,22 @@
 <div class="row vh100 section_four padding-50-px">
     <div class="col-md-12 card_container background_default">
-        <div class="section_four_card text-white">
-            <h5>ANTIGUA Y SUS ALREDEDORES</h5>
+        <div class="section_four_card text-white d-flex flex-column align-items-center justify-content-center">
+            <div class="text_content p-100">
+                <div class="logo_line background_default mb-2"></div>
+
+                <h2><strong>ANTIGUA Y SUS ALREDEDORES</strong></h2>
+
+                <p class="text-white">
+                    Nullam egestas lectus eget posuere elementum. Aliquam urna erat, fermentum id rutrum sed, feugiat id
+                    magna. In justo tellus, ultricies et consequat non, consectetur in diam. Suspendisse dui libero,
+                    pharetra porttitor hendrerit fermentum, vehicula sed arcu. Maecenas laoreet facilisis vulputate.
+                    Quisque
+                    consequat, metus eu condimentum lobortis, mauris turpis aliquet lorem, eu consequat sem ante et
+                    magna.
+                    Nulla vitae varius risus. Donec sem ligula, varius sed leo id, imperdiet aliquam arcu. Integer eget
+                    auctor sem, at commodo erat.
+                </p>
+            </div>
         </div>
     </div>
 </div>
---
+++ resources/views/sections/section_six.blade.php
@@ -0,0 +1,9 @@
+<div class="row vh100 section_two padding-50-px text-center">
+    <div class="col-md-12">
+        <div class="logo_line background_default mb-2"></div>
+        <h2><strong>SALA DE PRENSA</strong></h2>
+        <h3 class="padding-unset text_secondary_color">Lea las opiniones de nuestros huéspedes</h3>
+    </div>
+    <div class="col-md-6"></div>
+    <div class="col-md-6"></div>
+</div>
