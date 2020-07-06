f83402d08c13862ed1e3d7b14ae4e412b1066068
Jose Sicajan <jlsicajan@gmail.com>
2020-07-06 03:20:23 -0600

    Changes made

--- resources/views/sections/section_three.blade.php
+++ resources/views/sections/section_three.blade.php
@@ -9,7 +9,7 @@
             class="inside_container bg-white text-center section_three_box_shadow h-100 d-flex flex-column justify-content-center">
             <h3 class="text_secondary_color"><i class="fa fa-leaf text_secondary_color f_0_8"></i><strong> Parqueo
                     gratuito </strong><i class="fa fa-leaf text_secondary_color f_0_8"></i></h3>
-            <h4>Un lugar seguro donde puedes dejar tu vehículo.</h4>
+            <h5>Un lugar seguro donde puedes dejar tu vehículo.</h5>
         </div>
     </div>
     <div class="col-md-6 p-2 h-236">
@@ -18,7 +18,7 @@
             <i class="fa fa-wine-glass-alt text-light medium_icon"></i>
             <h3 class="text-light"><i class="fa fa-leaf text-white f_0_8"></i><strong> Desayunos </strong><i
                     class="fa fa-leaf text-white f_0_8"></i></h3>
-            <h4 class="text-white">En nuestro restaurante le servimos desayunos, almuerzos y cenas nacionales. Disfrute una buena comida con una agradable vista a los volcanes</h4>
+            <h5 class="text-white">En nuestro restaurante le servimos desayunos, almuerzos y cenas nacionales. Disfrute una buena comida con una agradable vista a los volcanes</h5>
         </div>
     </div>
     <div class="col-md-6 p-2 h-236">
@@ -27,15 +27,15 @@
             <i class="fa fa-wifi text-light medium_icon"></i>
             <h3 class="text-light"><i class="fa fa-leaf text-white f_0_8"></i><strong> Wifi gratis </strong><i
                     class="fa fa-leaf text-white f_0_8"></i></h3>
-            <h4 class="text-white">Todas las habitacions con internet WIFI para su mayor comodidad.</h4>
+            <h5 class="text-white">Todas las habitacions con internet WIFI para su mayor comodidad.</h5>
         </div>
     </div>
     <div class="col-md-6 p-2 h-236">
         <div
             class="inside_container bg-white text-center section_three_box_shadow h-100 d-flex flex-column justify-content-center">
             <h3 class="text_secondary_color"><i class="fa fa-leaf text_secondary_color f_0_8"></i><strong> Reserve </strong><i class="fa fa-leaf text_secondary_color f_0_8"></i></h3>
-            <h4>Para mas informacion, sera un gusto atenderle en el numero: <br> <a href="tel:+50244893766">+502 44893766</a> o al correo:
-                <a href="mailto:hoteljardinlapaz@gmail.com">hoteljardinlapaz@gmail.com</a> le atenderos los mas pronto posible.</h4>
+            <h5>Para mas informacion, sera un gusto atenderle en el numero: <br> <a href="tel:+50244893766">+502 44893766</a> o al correo:
+                <a href="mailto:hoteljardinlapaz@gmail.com">hoteljardinlapaz@gmail.com</a> le atenderos los mas pronto posible.</h5>
         </div>
     </div>
     <div class="col-md-12 mt-5 mb-5">
@@ -46,7 +46,7 @@
         <a href="https://www.booking.com/" class="no-style list-unstyled cursor-pointer">
             <div class="text-center section_three_box_shadow h-100 bg-dark d-flex flex-column justify-content-center">
                 <i class="fa fa-calendar text-white medium_icon"></i><br>
-                <h4 class="text-white"><strong>VERIFICAR DISPONIBILIDAD Y RESERVAR EN LINEA</strong></h4>
+                <h5 class="text-white"><strong>VERIFICAR DISPONIBILIDAD Y RESERVAR EN LINEA</strong></h5>
             </div>
         </a>
     </div>
@@ -54,7 +54,7 @@
         <a href="tel:+50244893766" class="no-style list-unstyled cursor-pointer">
             <div class="text-center section_three_box_shadow h-100 bg-dark d-flex flex-column justify-content-center">
                 <i class="fa fa-phone text-white medium_icon"></i><br>
-                <h4 class="text-white"><strong>LLAMENOS (+502) 4489 3766</strong></h4>
+                <h5 class="text-white"><strong>LLAMENOS (+502) 4489 3766</strong></h5>
             </div>
         </a>
     </div>
